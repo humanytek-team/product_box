064fa7e0ad049d61e2cddd8c2558326ab15fb743
Manuel Marquez <buzondemam@gmail.com>
2017-10-07 12:44:31 -0400

    [IMP] Updates version to new release 9.0.1.0.0 in CHANGELOG.

--- __openerp__.py
+++ __openerp__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Product Boxes Manager',
-    'version': '9.0.0.1.0',
+    'version': '9.0.1.0.0',
     'category': 'Product',
     'summary': 'Product Boxes Manager',
     'author': 'Humanytek',
